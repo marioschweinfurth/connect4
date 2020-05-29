923995fe60da67e1a2be0904d2112a7bd7827933
MarioS <mario.schweinfurth@gmail.com>
2020-05-29 14:08:50 -0700

    arregle el tablero

--- connect4.py
+++ connect4.py
@@ -7,47 +7,46 @@ escalaT2 = re.compile('[1][0][x][1][0]')
 
 def tablero(x,y):
     tablero = []
-    for i in range(0,x+2):
+    for i in range(0,x):
         tablero.append([])
-        for j in range(0,y+2):
-            if i == 0:
-                tablero[i].append(j)
-            else:
-                tablero[i].append(' ')
+        for j in range(0,y):
+            tablero[i].append(' ')
     return tablero
 
+def jugada(x):
 
 
 print('ingrese el tamanio del tablero. tome en cuenta que el tablero no puede ser mas pequenio de 5x6 y no puede ser mas grande de 10x10')
 escala = input('tamanio: ')
 if escalaT.fullmatch(escala):
-    lineas = int(escala[0])
-    columnas = int(escala[2])
-    tablero = tablero(lineas+1,columnas)
-    for i in range(0,lineas+1):
-        for j in range(0,columnas+1):
+    li = int(escala[0])
+    colu = int(escala[2])
+    tablero = tablero(li,colu)
+    for i in range(0,li):
+        for j in range(0,colu):
             if j == 0:
                 print('|', end='')
                 print(tablero[i][j], end=' |')
-            elif j == columnas:
+            elif j == colu-1:
                 print(tablero[i][j], end=' |\n')
             else:
                 print(tablero[i][j], end=' |')
-    for i in range(0,columnas+1):
+    for i in range(0,colu+1):
         print('---', end='')
 elif escalaT2.fullmatch(escala):
-    lineas = int(escala[0:2])
-    columnas = int(escala[3:5])
-    tablero = tablero(lineas+1,columnas)
-    for i in range(0,lineas+1):
-        for j in range(0,columnas+1):
+    li = int(escala[0:2])
+    colu = int(escala[3:5])
+    tablero = tablero(li,colu)
+    for i in range(0,li):
+        for j in range(0,colu):
             if j == 0:
                 print('|', end='')
                 print(tablero[i][j], end=' |')
-            elif j == columnas:
+            elif j == colu-1:
                 print(tablero[i][j], end=' |\n')
             else:
                 print(tablero[i][j], end=' |')
-
+    for i in range(0,colu+1):
+        print('---', end='')
 else:
     print('tamanio invalido')
