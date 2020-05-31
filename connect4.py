4d1c5f553c4708296e746238a2bd410778d7e558
MarioS <mario.schweinfurth@gmail.com>
2020-05-30 18:53:35 -0700

    agrege distintas fichas a la funcion jugada

--- connect4.py
+++ connect4.py
@@ -3,14 +3,9 @@ import re
 
 escalaT = re.compile('[5-9][x][6-9]')
 escalaT2 = re.compile('[1][0][x][1][0]')
-'''
-ama = "\033[0;33;01m \n"
-ver = "\033[0;32;01m \n"
-blanco = '\033[0;37;01m \n'
-pieza = 'O'
-print(ama , pieza, ver, pieza, blanco)
-#print(piezaA)
-'''
+piezaR = "\033[0;31;01mO\033[0;37;01m"
+piezaV = "\033[0;32;01mO\033[0;37;01m"
+
 def tablero(x,y):
     tablero = []
     for i in range(0,x):
@@ -19,16 +14,23 @@ def tablero(x,y):
             tablero[i].append(' ')
     return tablero
 
-def jugada(x):
-    global tablero
+def jugada(x,y):
+    global tablero, piezaR, piezaV
     diccionario = {1:0,2:1,3:2,4:3,5:4,6:5,7:6,8:7,9:8,10:9}
     x = diccionario[x]
     tablero.reverse()
-    for i in range(0,len(tablero)+1):
-        if tablero[i][x] == ' ':
-            tablero[i].pop(x)
-            tablero[i].insert(x,'O')
-            break
+    if y == 0:
+        for i in range(0,len(tablero)+1):
+            if tablero[i][x] == ' ':
+                tablero[i].pop(x)
+                tablero[i].insert(x,piezaR)
+                break
+    else:
+        for i in range(0,len(tablero)+1):
+            if tablero[i][x] == ' ':
+                tablero[i].pop(x)
+                tablero[i].insert(x,piezaV)
+                break
     tablero.reverse()
 
 def desplegart(x):
@@ -88,6 +90,7 @@ def main():
     global j1, j2, empa
     while True:
         print("1------HvsH")
+        print('4. salir')
         try:
             opcion = int(input(""))
             if opcion == 1:
@@ -96,12 +99,11 @@ def main():
                 n2 = input("Ingrese el 2 jugador: ")
                 j = random.randint(0,1)
                 hvsh(n1,n2,j)
+            elif opcion == 4:
+                print('bye')
+                break
+            else:
+                print('opcion invalida')
         except ValueError:
             print("Nel")
 main()
-'''
-tablero = tablero(5,6)
-desplegart('5x6')
-jugada(4)
-desplegart('5x6')
-'''
